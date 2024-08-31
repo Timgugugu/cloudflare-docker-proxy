6b2e1cb396761c698ab84ce117041aedcb044c03
Timgugugu <100938474+Timgugugu@users.noreply.github.com>
2024-08-31 14:16:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.tis.icu": dockerHub,
+  "quay.tis.icu": "https://quay.io",
+  "gcr.tis.icu": "https://gcr.io",
+  "k8s-gcr.tis.icu": "https://k8s.gcr.io",
+  "k8s.tis.icu": "https://registry.k8s.io",
+  "ghcr.tis.icu": "https://ghcr.io",
+  "cloudsmith.tis.icu": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.lixd.xyz": dockerHub,
